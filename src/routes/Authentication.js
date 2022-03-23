e90d5e3b9db8ac9365e7489da57e4cdc111aff12
Hennization <shy6295@ewhain.net>
2022-03-23 16:34:49 +0900

    2 1 Login Form

--- src/routes/Authentication.js
+++ src/routes/Authentication.js
@@ -1,16 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
 
-const Authentication = () => (
-  <div>
-    <form>
-      <input type="text" placeholder="Email" required />
-      <input type="password" placeholder="Password" required />
-      <input type="submit" value="Log In" />
-    </form>
+const Authentication = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const onChange = (e) => {
+    const {
+      target: { name, value },
+    } = e;
+    if (name === "email") {
+      setEmail(value);
+    } else if (name === "password") {
+      setPassword(value);
+    }
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+  };
+  return (
     <div>
-      <button>Continue with Google</button>
-      <button>Continue with Github</button>
+      <form onSubmit={onSubmit}>
+        <input
+          name="email"
+          type="text"
+          placeholder="Email"
+          value={email}
+          onChange={onChange}
+          required
+        />
+        <input
+          name="password"
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={onChange}
+          required
+        />
+        <input type="submit" value="Log In" />
+      </form>
+      <div>
+        <button>Continue with Google</button>
+        <button>Continue with Github</button>
+      </div>
     </div>
-  </div>
-);
+  );
+};
 export default Authentication;
